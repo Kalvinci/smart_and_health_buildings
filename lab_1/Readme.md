72935ebb5de88dc29c482dfa64cb7d48144684dd
Kalvinci <kalyanasundharam97@gmail.com>
2021-09-10 07:53:55 -0400

    Lab 1

---
+++ lab_1/Readme.md
@@ -0,0 +1,4 @@
+### Kalvinci
+- Elmo
+- Rock
+- Morning bird
---
+++ lab_1/good_bye.py
@@ -0,0 +1,8 @@
+import random
+
+def goodbye():
+	for i in range(random.randint(1,25)):
+		print("good bye cruel world")
+
+if __name__ == "__main__":
+	goodbye()
